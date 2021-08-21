0f560e1875331aa85bce3ded362b757023453cdd
Anikkhan-web <anikkhan9521@gmail.com>
2021-08-21 03:28:09 -0700

    function with functionality

--- app.js
+++ app.js
@@ -1,67 +1,86 @@
 
 
+function bgColor(){
+
+}
+
+function clickWithElement(withElement,price){
+    const memoryCost = document.getElementById( withElement +'-cost')
+    memoryCost.innerText = price;
+    allCost()
+}
+
+function allCost(){
+   
+    const bestPrice = document.getElementById('best-price').innerText;
+
+    const memoryPrice = document.getElementById('memory-cost').innerText ;
+   
+    const storagePrice = document.getElementById('storage-cost').innerText;
+    
+    const deliveryPrice = document.getElementById('delivery-cost').innerText;
+
+    const totalPrice = document.getElementById('total-price').innerText = parseInt(bestPrice) +parseInt(memoryPrice) +parseInt(storagePrice) + parseInt(deliveryPrice);
+
+}
+
 
 document.getElementById('memoryFrist-field').addEventListener('click',function(){
+    clickWithElement('memory',0);
 
-    const memoryCost = document.getElementById('memory-cost')
-memoryCost.innerText = "0"
+    allCost();
 
-    console.log("newbee");
-
-
-
+    document.getElementById('total-price').innerText = memory8gbTotal
 
 })
 
 document.getElementById('memorySecond-field').addEventListener('click',function(){
+    clickWithElement('memory',180);
 
-const memoryCost = document.getElementById('memory-cost')
-memoryCost.innerText = "180"
-    console.log("newbee");
+    allCost();
 
+    document.getElementById('total-price').innerText = memory16gbTotal
 
 })
-
 document.getElementById('storageFrist-field').addEventListener('click',function(){
+    clickWithElement('storage',0);
 
-    const memoryCost = document.getElementById('storage-cost')
-    memoryCost.innerText = "0"
-    console.log("newbee");
+    allCost();
 
+    document.getElementById('total-price').innerText = storage256Total
 
 })
 
 document.getElementById('storageSecond-field').addEventListener('click',function(){
+ckWithElement('storage',100);
 
-    const memoryCost = document.getElementById('storage-cost')
-    memoryCost.innerText = "100"
-    console.log("newbee");
+    allCost();
+    document.getElementById('total-price').innerText = storage512Total
 
-
+    
 })
 
 document.getElementById('storageThird-field').addEventListener('click',function(){
-    const memoryCost = document.getElementById('storage-cost')
-    memoryCost.innerText = "180"
+    clickWithElement('storage',180);
+    
+    allCost();
 
-    console.log("newbee");
+    document.getElementById('total-price').innerText = storage1tbTotal
 
 
 })
 document.getElementById('deliveryFrist-field').addEventListener('click',function(){
-    const memoryCost = document.getElementById('delivery-cost')
-    memoryCost.innerText = "0"
+    clickWithElement('delivery',0);
 
-    console.log("newbee");
+    allCost();
 
+    document.getElementById('total-price').innerText = deliveryfree
 
 })
-
 document.getElementById('deliverySecond-field').addEventListener('click',function(){
-    const memoryCost = document.getElementById('delivery-cost')
-    memoryCost.innerText = "20"
+    clickWithElement('delivery',20);
 
-    console.log("newbee");
+    allCost();
 
-
+    document.getElementById('total-price').innerText = deliveryfree20
 })
