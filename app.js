070d53b6e09b791311c669ff7f32283f868317ff
Anikkhan-web <anikkhan9521@gmail.com>
2021-08-21 00:14:29 -0700

    ui stracture

---
+++ app.js
@@ -0,0 +1,67 @@
+
+
+
+document.getElementById('memoryFrist-field').addEventListener('click',function(){
+
+    const memoryCost = document.getElementById('memory-cost')
+memoryCost.innerText = "0"
+
+    console.log("newbee");
+
+
+
+
+})
+
+document.getElementById('memorySecond-field').addEventListener('click',function(){
+
+const memoryCost = document.getElementById('memory-cost')
+memoryCost.innerText = "180"
+    console.log("newbee");
+
+
+})
+
+document.getElementById('storageFrist-field').addEventListener('click',function(){
+
+    const memoryCost = document.getElementById('storage-cost')
+    memoryCost.innerText = "0"
+    console.log("newbee");
+
+
+})
+
+document.getElementById('storageSecond-field').addEventListener('click',function(){
+
+    const memoryCost = document.getElementById('storage-cost')
+    memoryCost.innerText = "100"
+    console.log("newbee");
+
+
+})
+
+document.getElementById('storageThird-field').addEventListener('click',function(){
+    const memoryCost = document.getElementById('storage-cost')
+    memoryCost.innerText = "180"
+
+    console.log("newbee");
+
+
+})
+document.getElementById('deliveryFrist-field').addEventListener('click',function(){
+    const memoryCost = document.getElementById('delivery-cost')
+    memoryCost.innerText = "0"
+
+    console.log("newbee");
+
+
+})
+
+document.getElementById('deliverySecond-field').addEventListener('click',function(){
+    const memoryCost = document.getElementById('delivery-cost')
+    memoryCost.innerText = "20"
+
+    console.log("newbee");
+
+
+})
