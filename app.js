2bc1decea830f246a79f7cdabc43fd764f66b607
Anikkhan-web <anikkhan9521@gmail.com>
2021-08-21 15:06:23 -0700

    site almost done

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 function clickWithElement(withElement,price){
     const memoryCost = document.getElementById( withElement +'-cost')
     memoryCost.innerText = price;
@@ -7,105 +5,61 @@ function clickWithElement(withElement,price){
 }
 
 function allCost(){
-   
     const bestPrice = document.getElementById('best-price').innerText;
-
     const memoryPrice = document.getElementById('memory-cost').innerText ;
-   
     const storagePrice = document.getElementById('storage-cost').innerText;
-    
     const deliveryPrice = document.getElementById('delivery-cost').innerText;
-
     const totalPrice = document.getElementById('total-price')
-     totalPrice.innerText = parseInt(bestPrice) +parseInt(memoryPrice) +parseInt(storagePrice) + parseInt(deliveryPrice);
-    // return totalPrice;
+    totalPrice.innerText = parseInt(bestPrice) +parseInt(memoryPrice) +parseInt(storagePrice) + parseInt(deliveryPrice);
+   
 }
-
-
 
 document.getElementById('memoryFrist-field').addEventListener('click',function(){
     clickWithElement('memory',0);
-
     allCost();
-
-    document.getElementById('total-price').innerText ;
-
+    
 })
 
 document.getElementById('memorySecond-field').addEventListener('click',function(){
     clickWithElement('memory',180);
-
     allCost();
-
-    document.getElementById('total-price').innerText
-
+    
 })
 document.getElementById('storageFrist-field').addEventListener('click',function(){
     clickWithElement('storage',0);
-
     allCost();
-
-    document.getElementById('total-price').innerText 
-
+    
 })
-
 document.getElementById('storageSecond-field').addEventListener('click',function(){
     clickWithElement('storage',100);
-
-    allCost();
-
-    document.getElementById('total-price').innerText
+    allCost();   
 
 })
-
-    
-
-
 document.getElementById('storageThird-field').addEventListener('click',function(){
     clickWithElement('storage',180);
-    
     allCost();
     
-    document.getElementById('total-price').innerText 
-
-
 })
 document.getElementById('deliveryFrist-field').addEventListener('click',function(){
     clickWithElement('delivery',0);
     allCost();
-    document.getElementById('total-price').innerText
-
+     
 })
-
 document.getElementById('deliverySecond-field').addEventListener('click',function(){
     clickWithElement('delivery',20);
-
     allCost();
 
-    document.getElementById('total-price').innerText
 })
-
 
 document.getElementById('submit-pomo').addEventListener('click',function(){
-
     const promoInput = document.getElementById('promo-input')
-   const neww = document.getElementById('total-price').innerText 
+    const totalPriceText = document.getElementById('total-price').innerText 
    
-if(promoInput.value == 'stevekaku'){
-    const discount = parseInt(neww - (neww*20 / 100));
+    if(promoInput.value == 'stevekaku'){
+    const discount = totalPriceText - (totalPriceText* 20 / 100);
     document.getElementById('total-discount').innerText = discount
     promoInput.value = '';
-   
-
-}    
-
-    
-//     document.getElementById('total-price').innerText = in
-    
-//   const grandtotalPrice = totalPrice/2
-//   console.log(grandtotalPrice)
-
- 
-})
-
-
+    }else{
+        promoInput.value = 'Reload and Get Promo'
+    }
+})  
