54c965b5ff947166834def262c32e9ec2f60f345
Anikkhan-web <anikkhan9521@gmail.com>
2021-08-21 20:46:25 -0700

    update

--- app.js
+++ app.js
@@ -1,53 +1,58 @@
 function clickWithElement(withElement,price){
     const memoryCost = document.getElementById( withElement +'-cost')
     memoryCost.innerText = price;
-    allCost()
+    grandTotalCost()
 }
 
-function allCost(){
+function grandTotalCost(){
     const bestPrice = document.getElementById('best-price').innerText;
     const memoryPrice = document.getElementById('memory-cost').innerText ;
     const storagePrice = document.getElementById('storage-cost').innerText;
     const deliveryPrice = document.getElementById('delivery-cost').innerText;
-    const totalPrice = document.getElementById('total-price')
+    const totalPrice = document.getElementById('total-price');
+    
     totalPrice.innerText = parseInt(bestPrice) +parseInt(memoryPrice) +parseInt(storagePrice) + parseInt(deliveryPrice);
-   
+
+    // const totalPrice = document.getElementById('total-price');
+
+    const grandTotal =  document.getElementById('grand-total');
+     grandTotal.innerText = totalPrice.innerText ;
 }
 
 document.getElementById('memoryFrist-field').addEventListener('click',function(){
     clickWithElement('memory',0);
-    allCost();
+    grandTotalCost();
     
 })
 
 document.getElementById('memorySecond-field').addEventListener('click',function(){
     clickWithElement('memory',180);
-    allCost();
+    grandTotalCost();
     
 })
 document.getElementById('storageFrist-field').addEventListener('click',function(){
     clickWithElement('storage',0);
-    allCost();
+    grandTotalCost();
     
 })
 document.getElementById('storageSecond-field').addEventListener('click',function(){
     clickWithElement('storage',100);
-    allCost();   
+    grandTotalCost();   
 
 })
 document.getElementById('storageThird-field').addEventListener('click',function(){
     clickWithElement('storage',180);
-    allCost();
+    grandTotalCost();
     
 })
 document.getElementById('deliveryFrist-field').addEventListener('click',function(){
     clickWithElement('delivery',0);
-    allCost();
+    grandTotalCost();
      
 })
 document.getElementById('deliverySecond-field').addEventListener('click',function(){
     clickWithElement('delivery',20);
-    allCost();
+    grandTotalCost();
 
 })
 
@@ -57,9 +62,10 @@ document.getElementById('submit-pomo').addEventListener('click',function(){
    
     if(promoInput.value == 'stevekaku'){
     const discount = totalPriceText - (totalPriceText* 20 / 100);
-    document.getElementById('total-discount').innerText = discount
+    document.getElementById('grand-total').innerText = discount
     promoInput.value = '';
     }else{
         promoInput.value = 'Reload and Get Promo'
     }
+    
 })  
